9fe447d829853bf7823a8b7eba86154f3be240e3
daniel-SP <me@dnhaugen.com>
2022-02-02 08:47:01 -0800

    refine api routes

--- api/skateparks/controllers/allskateparksdingdong.js
+++ api/skateparks/controllers/allskateparksdingdong.js
@@ -17,6 +17,21 @@ module.exports = {
         'slug': index.slug
       }); 
     });
+
+    function compare(a, b) {
+      const bandA = a.name.toUpperCase();
+      const bandB = b.name.toUpperCase();
+      let comparison = 0;
+      if (bandA > bandB) {
+        comparison = 1;
+      } else if (bandA < bandB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    newAnswer.sort(compare);
+
     ctx.send(newAnswer);
 
 
